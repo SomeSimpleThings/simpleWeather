ac199144c1cfde881973fb7e85b828fb40edf7cb
Evgeny <capevery@gmail.com>
2021-01-27 23:38:49 +0300

    hardcode api key

--- app/src/main/java/com/geekbrains/simpleweather/api/ApiConstants.java
+++ app/src/main/java/com/geekbrains/simpleweather/api/ApiConstants.java
@@ -1,7 +1,5 @@
 package com.geekbrains.simpleweather.api;
 
-import com.geekbrains.simpleweather.BuildConfig;
-
 public class ApiConstants {
 
     public static final String LOCALE = "ru";
@@ -11,14 +9,19 @@ public class ApiConstants {
     private static final String FORECAST_URL = BASE_URL + "forecast?" + QUERY_PARAMS;
     private static final String CURRENT_WEATHER_URL = BASE_URL + "weather?" + QUERY_PARAMS;
 
+    public static final String OPENWEATHER_API_KEY = "aeda831c879480708dfb5e9bc4e5f74d";
+
+
     private ApiConstants() {
     }
 
     public static String getCurrentWeatherUrl(String city) {
-        return String.format(CURRENT_WEATHER_URL, city, UNITS, LOCALE, BuildConfig.OPENWEATHER_API_KEY);
+//        return String.format(CURRENT_WEATHER_URL, city, UNITS, LOCALE, BuildConfig.OPENWEATHER_API_KEY);
+        return String.format(CURRENT_WEATHER_URL, city, UNITS, LOCALE, OPENWEATHER_API_KEY);
     }
 
     public static String getForecastUrl(String city) {
-        return String.format(FORECAST_URL, city, UNITS, LOCALE, BuildConfig.OPENWEATHER_API_KEY);
+//        return String.format(FORECAST_URL, city, UNITS, LOCALE, BuildConfig.OPENWEATHER_API_KEY);
+        return String.format(FORECAST_URL, city, UNITS, LOCALE, OPENWEATHER_API_KEY);
     }
 }
